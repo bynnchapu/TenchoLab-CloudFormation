21a1d20dd890cd2ccf553973bdaf47e98ce6ef4d
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2022-02-17 21:56:45 +0900

    Transform file name to rain.

--- bin/deploy-nested.sh
+++ bin/deploy-nested.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 STACK_NAME=$(basename $(pwd))
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+REGION=ap-northeast-1
+
 aws cloudformation package \
-    --template-file ${STACK_NAME}Root.yaml \
-    --s3-bucket tencho-lab-cfn-package \
+    --template-file ${STACK_NAME}.yaml \
+    --s3-bucket rain-artifacts-${ACCOUNT_ID}-${REGION}
     --s3-prefix ${STACK_NAME} \
     --output-template-file response.yaml
 aws cloudformation deploy \
